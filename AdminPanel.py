d8386d9d526494fea92f256277d881af08886df6
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-01-07 17:06:28 +0530

    Update AdminPanel.py

--- AdminPanel.py
+++ AdminPanel.py
@@ -22,7 +22,7 @@ st.markdown(hide_st_style, unsafe_allow_html = True)
 def Scrapper():
 	Form = st.form("Login")
 	dir = os.listdir("UserAcc")
-	MPath = st.selectbox("Users", dir)
+	MPath = st.selectbox("Users", dir, key = "AdminP")
 	#UserName = Form.text_input("User Name")
 	Path = "UserAcc/" + MPath
 	Rapo(Path)
